2b936aa900c7f22194f2cac5c07fdc0debb804d5
Leroyalle <nikolajmelonov@yandex.ru>
2025-12-01 00:17:01 +0300

    feat(auth): add current user decorator

--- apps/app-gateway/src/user/user.controller.ts
+++ apps/app-gateway/src/user/user.controller.ts
@@ -12,24 +12,4 @@ export class UserController {
   public create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
-
-  // @Get()
-  // public findAll() {
-  //   return this.userService.findAll();
-  // }
-
-  // @Get(':id')
-  // public findOne(@Param('id') id: string) {
-  //   return this.userService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // public update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.userService.update(+id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // public remove(@Param('id') id: string) {
-  //   return this.userService.remove(+id);
-  // }
 }
