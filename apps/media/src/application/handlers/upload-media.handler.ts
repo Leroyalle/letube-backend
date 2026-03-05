ee656da2dde2865827043f8b3ee9782939b998d9
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-05 23:38:43 +0300

    feat(media): add upload handler

---
+++ apps/media/src/application/handlers/upload-media.handler.ts
@@ -0,0 +1,18 @@
+import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
+import { UploadMediaCommand } from '../commands/upload-media.command';
+import type { FileStoragePort } from '../storage/file-storage.port';
+import { Inject } from '@nestjs/common';
+import { FILE_STORAGE_TOKEN } from '../storage/file-storage.token';
+
+@CommandHandler(UploadMediaCommand)
+export class UploadMediaHandler implements ICommandHandler<UploadMediaCommand> {
+  constructor(
+    @Inject(FILE_STORAGE_TOKEN)
+    private readonly fileStorageService: FileStoragePort,
+  ) {}
+
+  public async execute(command: UploadMediaCommand) {
+    const url = await this.fileStorageService.getUploadUrl(command.filename);
+    return url;
+  }
+}
