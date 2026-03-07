e13ab18351d9767caa845ab3fb569b67bcd467b1
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-07 12:21:23 +0300

    refactor(media): improve upload complete handler by function decomposition

--- apps/media/src/application/handlers/upload-complete.handler.ts
+++ apps/media/src/application/handlers/upload-complete.handler.ts
@@ -1,64 +1,33 @@
-import { spawn } from 'child_process';
-import { createReadStream, createWriteStream, promises } from 'fs';
-import { join } from 'path';
-import { pipeline } from 'stream/promises';
-
+import { Inject } from '@nestjs/common';
 import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
 
 import { UploadCompleteCommand } from '../commands/upload-complete.command';
 import type { FileStoragePort } from '../storage/file-storage.port';
+import { FILE_STORAGE_TOKEN } from '../storage/file-storage.token';
+import type { TempFolderPort } from '../temp-folder/temp-folder.port';
+import { TEMP_FOLDER_TOKEN } from '../temp-folder/temp-folder.token';
+import type { VideoProcessorPort } from '../video/video-processor.port';
+import { VIDEO_PROCESSOR_TOKEN } from '../video/video-processor.token';
 
 // TODO: вынести в воркер
 @CommandHandler(UploadCompleteCommand)
 export class UploadCompleteHandler implements ICommandHandler<UploadCompleteCommand> {
-  constructor(private readonly fileStorageService: FileStoragePort) {}
+  constructor(
+    @Inject(FILE_STORAGE_TOKEN)
+    private readonly fileStorageService: FileStoragePort,
+    @Inject(TEMP_FOLDER_TOKEN)
+    private readonly tempFolderService: TempFolderPort,
+    @Inject(VIDEO_PROCESSOR_TOKEN)
+    private readonly videoProcessorService: VideoProcessorPort,
+  ) {}
+
   public async execute(command: UploadCompleteCommand) {
-    const inputPath = `/tmp/video/${command.s3Key}`;
-    const outputPath = `/tmp/video/${command.s3Key}.m3u8`;
     const readable = await this.fileStorageService.get(command.s3Key);
+    if (!readable) throw new Error('Файл не найден');
 
-    if (!readable) return;
+    const { inputDir, outputDir } = this.tempFolderService.prepare(command.s3Key);
+    await this.videoProcessorService.cup(inputDir, outputDir, readable);
 
-    const fileStream = createWriteStream(inputPath);
-
-    await pipeline(readable, fileStream);
-
-    await new Promise<void>((resolve, reject) => {
-      const ffmpeg = spawn(
-        'C:\\Users\\nikol\\Downloads\\ffmpeg-2026-03-05-git-74cfcd1c69-essentials_build\\ffmpeg-2026-03-05-git-74cfcd1c69-essentials_build\\bin\\ffmpeg.exe',
-        [
-          '-i',
-          inputPath,
-          '-f',
-          'hls',
-          '-hls_segment_filename',
-          `/tmp/video/${command.s3Key}/segment%03d.ts`,
-          outputPath,
-        ],
-      );
-
-      ffmpeg.on('close', code => {
-        if (code === 0) resolve();
-        else reject(new Error(`ffmpeg exited with code ${code}`));
-      });
-
-      ffmpeg.on('error', reject);
-    });
-
-    const files = await promises.readdir(`/tmp/video/${command.s3Key}`);
-
-    const concurrency = 10;
-
-    for (let i = 0; i < files.length; i += concurrency) {
-      const batch = files.slice(i, i + concurrency);
-
-      await Promise.all(
-        batch.map(async file => {
-          const pathFile = join('/temp/video', command.s3Key, file);
-          const stream = createReadStream(pathFile);
-          await this.fileStorageService.put(command.s3Key, stream);
-        }),
-      );
-    }
+    await this.fileStorageService.uploadFolder(`/tmp/video/${command.s3Key}`, command.s3Key);
   }
 }
