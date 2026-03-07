57824d83e29cc3f81b179556aa0dbf64d79a342f
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-07 23:28:29 +0300

    feat(media): add find by key and update video repository methods

--- apps/media/src/infrastructure/persistence/db/prisma-video.repository.ts
+++ apps/media/src/infrastructure/persistence/db/prisma-video.repository.ts
@@ -1,14 +1,20 @@
 import { Video } from 'apps/media/src/domain/entities/video.entity';
 import type { VideoRepositoryPort } from 'apps/media/src/domain/interfaces/video-repository.port';
 
-import type { PrismaService } from '../../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
 
+import type { Video as PrismaVideo } from '../../../../__generated__/prisma';
+import { PrismaService } from '../../prisma/prisma.service';
+
+import { VideoMapper } from './video.mapper';
+
+@Injectable()
 export class PrismaVideoRepository implements VideoRepositoryPort {
   constructor(private readonly prismaService: PrismaService) {}
 
   public async create(video: Video) {
     const data = await this.prismaService.video.create({ data: video.props });
-    return new Video(data);
+    return this.mapToDomain(data);
   }
 
   public async findById(id: string) {
@@ -17,7 +23,33 @@ export class PrismaVideoRepository implements VideoRepositoryPort {
         id,
       },
     });
+    return this.mapToDomain(data);
+  }
 
-    return data ? new Video(data) : null;
+  public async findByKey(key: string) {
+    const data = await this.prismaService.video.findFirst({
+      where: {
+        sourceKey: key,
+      },
+    });
+    return this.mapToDomain(data);
+  }
+
+  public async update(video: Video) {
+    const data = await this.prismaService.video.update({
+      where: {
+        id: video.props.id,
+      },
+      data: video.props,
+    });
+    return this.mapToDomain(data);
+  }
+
+  private mapToDomain(data: PrismaVideo): Video;
+  private mapToDomain(data: PrismaVideo | null): Video | null;
+  private mapToDomain(data: PrismaVideo | null): Video | null {
+    if (!data) return null;
+    const domainData = VideoMapper.toDomain(data);
+    return new Video(domainData);
   }
 }
