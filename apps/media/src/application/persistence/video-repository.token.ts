03e92dd4a8d327d9d246f66de59814d178f3593e
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-07 23:29:49 +0300

    feat(media): register video repository

---
+++ apps/media/src/application/persistence/video-repository.token.ts
@@ -0,0 +1,1 @@
+export const VIDEO_REPOSITORY_TOKEN = Symbol('video_repository_token');
--- apps/media/src/media.module.ts
+++ apps/media/src/media.module.ts
@@ -6,8 +6,10 @@ import { CqrsModule } from '@nestjs/cqrs';
 
 import { commandHandlers } from './application/handlers/command-handlers';
 import { FILE_STORAGE_TOKEN } from './application/persistence/file-storage.token';
+import { VIDEO_REPOSITORY_TOKEN } from './application/persistence/video-repository.token';
 import { TEMP_FOLDER_TOKEN } from './application/temp-folder/temp-folder.token';
 import { VIDEO_PROCESSOR_TOKEN } from './application/video/video-processor.token';
+import { PrismaVideoRepository } from './infrastructure/persistence/db/prisma-video.repository';
 import { S3ClientService } from './infrastructure/persistence/s3/s3-client.service';
 import { TempFolderService } from './infrastructure/prepare/temp-folder/temp-folder.service';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
@@ -36,6 +38,10 @@ import { MediaController } from './presentation/http/media.controller';
     {
       provide: VIDEO_PROCESSOR_TOKEN,
       useClass: FfmpegVideoProcessor,
+    },
+    {
+      provide: VIDEO_REPOSITORY_TOKEN,
+      useClass: PrismaVideoRepository,
     },
     ...commandHandlers,
   ],
