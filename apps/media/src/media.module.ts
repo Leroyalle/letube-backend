7a228b15162a70c9f9b9e519ee0681ededdac32f
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-07 12:19:33 +0300

    fix(media): register services in module

--- apps/media/src/media.module.ts
+++ apps/media/src/media.module.ts
@@ -6,7 +6,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 
 import { commandHandlers } from './application/handlers/command-handlers';
 import { FILE_STORAGE_TOKEN } from './application/storage/file-storage.token';
+import { TEMP_FOLDER_TOKEN } from './application/temp-folder/temp-folder.token';
+import { VIDEO_PROCESSOR_TOKEN } from './application/video/video-processor.token';
+import { TempFolderService } from './infrastructure/prepare/temp-folder/temp-folder.service';
 import { S3ClientService } from './infrastructure/storage/s3/s3-client.service';
+import { FfmpegVideoProcessor } from './infrastructure/video/ffmpeg/ffmpeg-video-processor.service';
 import { MediaController } from './presentation/http/media.controller';
 
 @Module({
@@ -22,6 +26,14 @@ import { MediaController } from './presentation/http/media.controller';
     {
       provide: FILE_STORAGE_TOKEN,
       useClass: S3ClientService,
+    },
+    {
+      provide: TEMP_FOLDER_TOKEN,
+      useClass: TempFolderService,
+    },
+    {
+      provide: VIDEO_PROCESSOR_TOKEN,
+      useClass: FfmpegVideoProcessor,
     },
     ...commandHandlers,
   ],
