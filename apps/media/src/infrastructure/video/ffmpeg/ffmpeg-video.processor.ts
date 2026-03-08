5fb1a1e071857eaaeed9e3d91446fdd305583b0b
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-08 16:53:19 +0300

    feat(media): use video id and content type in processing pipeline

--- apps/media/src/infrastructure/video/ffmpeg/ffmpeg-video.processor.ts
+++ apps/media/src/infrastructure/video/ffmpeg/ffmpeg-video.processor.ts
@@ -1,5 +1,6 @@
 import { spawn } from 'child_process';
 import { createWriteStream } from 'fs';
+import { mkdir } from 'fs/promises';
 import { type Readable } from 'stream';
 import { pipeline } from 'stream/promises';
 
@@ -9,22 +10,27 @@ import type { VideoProcessorPort } from '../../../application/ports/video-proces
 
 @Injectable()
 export class FfmpegVideoProcessor implements VideoProcessorPort {
-  public async cup(inputPath: string, outputPath: string, source: Readable) {
-    const fileStream = createWriteStream(inputPath);
+  public async cut(inputPath: string, outputPath: string, source: Readable) {
+    await mkdir(inputPath, { recursive: true });
+    await mkdir(outputPath, { recursive: true });
 
-    await pipeline(source, fileStream);
+    const inputFile = `${inputPath}/input`;
+    const playlist = `${outputPath}/index.m3u8`;
+
+    const writeStream = createWriteStream(inputFile);
+    await pipeline(source, writeStream);
 
     return await new Promise<void>((resolve, reject) => {
       const ffmpeg = spawn(
         'C:\\Users\\nikol\\Downloads\\ffmpeg-2026-03-05-git-74cfcd1c69-essentials_build\\ffmpeg-2026-03-05-git-74cfcd1c69-essentials_build\\bin\\ffmpeg.exe',
         [
           '-i',
-          inputPath,
+          inputFile,
           '-f',
           'hls',
           '-hls_segment_filename',
-          `${inputPath}/segment%03d.ts`,
-          outputPath,
+          `${outputPath}/segment%03d.ts`,
+          playlist,
         ],
       );
 
