c2dbce261aedeb83b2265f35adb62308d0155360
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-09 20:14:33 +0300

    feat(media): add hls playlist and segment names constants

--- apps/media/src/infrastructure/video/ffmpeg/ffmpeg-video.processor.ts
+++ apps/media/src/infrastructure/video/ffmpeg/ffmpeg-video.processor.ts
@@ -1,3 +1,7 @@
+import {
+  HLS_PLAYLIST_NAME,
+  HLS_SEGMENT_NAME,
+} from 'apps/media/src/application/constants/hls.constant';
 import { spawn } from 'child_process';
 import { createWriteStream } from 'fs';
 import { mkdir } from 'fs/promises';
@@ -15,8 +19,8 @@ export class FfmpegVideoProcessor implements VideoProcessorPort {
     await mkdir(outputPath, { recursive: true });
 
     const inputFile = `${inputPath}/input`;
-    const playlist = `${outputPath}/index.m3u8`;
-    const segmentPath = `${outputPath}/segment%03d.ts`;
+    const playlist = `${outputPath}/${HLS_PLAYLIST_NAME}`;
+    const segmentPath = `${outputPath}/${HLS_SEGMENT_NAME}`;
 
     const writeStream = createWriteStream(inputFile);
     await pipeline(source, writeStream);
