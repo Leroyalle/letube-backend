c247b9ceb68f844d17df45917b220f424133b02d
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-11 23:05:42 +0300

    feat(media): add broker event bus port

--- apps/media/src/application/ports/tokens.ts
+++ apps/media/src/application/ports/tokens.ts
@@ -2,3 +2,4 @@ export const FILE_STORAGE_TOKEN = Symbol('file_storage');
 export const TEMP_FOLDER_TOKEN = Symbol('temp_folder_token');
 export const VIDEO_PROCESSOR_TOKEN = Symbol('video_processor_token');
 export const VIDEO_REPOSITORY_TOKEN = Symbol('video_repository_token');
+export const BROKER_EVENT_BUS_TOKEN = Symbol('broker_event_bus_token');
