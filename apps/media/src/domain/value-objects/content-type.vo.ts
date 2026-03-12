2dff3736ff629961ed1eb715b56a256db7558da8
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-13 00:07:57 +0300

    feat(media): rebuild content type vo by enum value

--- apps/media/src/domain/value-objects/content-type.vo.ts
+++ apps/media/src/domain/value-objects/content-type.vo.ts
@@ -1,15 +1,17 @@
-type ContentTypeValue = 'video' | 'image';
-export class ContentType {
-  private constructor(private readonly value: 'video' | 'image') {}
+import { ContentType as ContentTypeEnum } from '@contracts/media/enums/content-type.enum';
 
-  public static from(value: string) {
-    if (!this.isValid(value)) throw new Error('Invalid content type');
+// type ContentTypeValue = 'video' | 'image';
+export class ContentType {
+  private constructor(private readonly value: ContentTypeEnum) {}
+
+  public static from(value: ContentTypeEnum) {
+    // if (!this.isValid(value)) throw new Error('Invalid content type');
     return new ContentType(value);
   }
 
-  private static isValid(value: string): value is ContentTypeValue {
-    return ['video', 'image'].includes(value);
-  }
+  // private static isValid(value: ContentTypeEnum): value is ContentTypeValue {
+  //   return ['video', 'image'].includes(value);
+  // }
 
   public getValue() {
     return this.value;
