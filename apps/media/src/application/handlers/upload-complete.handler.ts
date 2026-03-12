ac9013951175fa0b83eca5837dfa8cf1828c9292
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-13 00:02:25 +0300

    feat(media): rebuild upload complete handler with broker emitting

--- apps/media/src/application/handlers/upload-complete.handler.ts
+++ apps/media/src/application/handlers/upload-complete.handler.ts
@@ -1,32 +1,22 @@
+import type { UploadCompletedDto } from '@contracts/media-worker/dto/upload-completed.dto';
+import { MEDIA_BROKER_QUEUES } from '@contracts/media/queues/broker.queues';
+
 import { Inject } from '@nestjs/common';
 import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
 
 import type { VideoRepositoryPort } from '../../domain/interfaces/video-repository.port';
 import { UploadCompleteCommand } from '../commands/upload-complete.command';
-import type { FileStoragePort } from '../ports/file-storage.port';
-import type { TempFolderPort } from '../ports/temp-folder.port';
-import {
-  FILE_STORAGE_TOKEN,
-  TEMP_FOLDER_TOKEN,
-  VIDEO_PROCESSOR_TOKEN,
-  VIDEO_REPOSITORY_TOKEN,
-} from '../ports/tokens';
-import type { VideoProcessorPort } from '../ports/video-processor.port';
-import { MediaStorageResolver } from '../services/media-storage-resolver/media-storage.resolver';
+import type { BrokerEventBusPort } from '../ports/broker-event-bus.port';
+import { BROKER_EVENT_BUS_TOKEN, VIDEO_REPOSITORY_TOKEN } from '../ports/tokens';
 
 // TODO: вынести в воркер
 @CommandHandler(UploadCompleteCommand)
 export class UploadCompleteHandler implements ICommandHandler<UploadCompleteCommand> {
   constructor(
-    @Inject(FILE_STORAGE_TOKEN)
-    private readonly fileStorageService: FileStoragePort,
-    @Inject(TEMP_FOLDER_TOKEN)
-    private readonly tempFolderService: TempFolderPort,
-    @Inject(VIDEO_PROCESSOR_TOKEN)
-    private readonly videoProcessorService: VideoProcessorPort,
     @Inject(VIDEO_REPOSITORY_TOKEN)
     private readonly videoRepository: VideoRepositoryPort,
-    private readonly mediaStorageResolver: MediaStorageResolver,
+    @Inject(BROKER_EVENT_BUS_TOKEN)
+    private readonly brokerEventBus: BrokerEventBusPort,
   ) {}
 
   public async execute(command: UploadCompleteCommand) {
@@ -34,36 +24,19 @@ export class UploadCompleteHandler implements ICommandHandler<UploadCompleteComm
 
     if (!video) throw new Error('Видео по ключу не найдено в базе данных');
 
-    if (!video.canStartProcessing()) throw new Error('Видео уже обрабатывается');
+    if (!video.canMakeUploaded()) throw new Error('Видео должно быть в состоянии UPLOADING');
 
-    video.changeStatus('PROCESSING');
-
-    const readable = await this.fileStorageService.get(video.props.sourceKey);
-
-    if (!readable) throw new Error('Файл в хранилище не найден');
-
-    const { inputDir, outputDir } = this.tempFolderService.prepare(
-      command.sourceId,
-      command.contentType,
-    );
-
-    await this.videoProcessorService.cut(inputDir, outputDir, readable);
-
-    const hlsFolderKey = this.mediaStorageResolver.createHlsFolderKey(
-      video.props.id,
-      command.contentType,
-    );
-
-    await this.fileStorageService.uploadFolder(outputDir, hlsFolderKey);
-
-    const playlistKey = this.mediaStorageResolver.createPlaylistKey(
-      video.props.id,
-      command.contentType,
-    );
-
-    video.setMasterKey(playlistKey);
+    video.changeStatus('UPLOADED');
 
     await this.videoRepository.update(video);
+
+    const dto: UploadCompletedDto = {
+      sourceKey: video.props.sourceKey,
+      sourceId: video.props.id,
+      contentType: command.contentType.getValue(),
+    };
+
+    this.brokerEventBus.emit(MEDIA_BROKER_QUEUES.uploaded, dto);
 
     return { sourceId: video.props.id };
   }
