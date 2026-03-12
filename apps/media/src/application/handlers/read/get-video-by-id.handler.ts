a092e65c92ec8390df052effe4a92773abd48ec2
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-13 01:02:46 +0300

    feat(media): add get video by id query and handler

---
+++ apps/media/src/application/handlers/read/get-video-by-id.handler.ts
@@ -0,0 +1,18 @@
+import type { VideoRepositoryPort } from 'apps/media/src/domain/interfaces/video-repository.port';
+
+import { Inject } from '@nestjs/common';
+import { type IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+
+import { VIDEO_REPOSITORY_TOKEN } from '../../ports/tokens';
+import { GetVideoByIdQuery } from '../../queries/get-video-by-id.query';
+
+@QueryHandler(GetVideoByIdQuery)
+export class GetVideoByIdHandler implements IQueryHandler<GetVideoByIdQuery> {
+  constructor(
+    @Inject(VIDEO_REPOSITORY_TOKEN) private readonly videoRepository: VideoRepositoryPort,
+  ) {}
+
+  public execute(query: GetVideoByIdQuery) {
+    return this.videoRepository.findById(query.id);
+  }
+}
