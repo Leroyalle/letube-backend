eafe13a0addb1e79356a9756490a001646754b5e
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-13 13:53:34 +0300

    feat(media): add channel adapter, token and port

--- apps/media/src/application/ports/tokens.ts
+++ apps/media/src/application/ports/tokens.ts
@@ -2,3 +2,4 @@ export const TEMP_FOLDER_TOKEN = Symbol('temp_folder_token');
 export const VIDEO_PROCESSOR_TOKEN = Symbol('video_processor_token');
 export const VIDEO_REPOSITORY_TOKEN = Symbol('video_repository_token');
 export const BROKER_EVENT_BUS_TOKEN = Symbol('broker_event_bus_token');
+export const CHANNEL_ADAPTER_TOKEN = Symbol('channel_adapter_token');
---
+++ apps/media/src/infrastructure/rpc/adapters/channel.rpc.adapter.ts
@@ -0,0 +1,24 @@
+import {
+  CHANNEL_PATTERNS,
+  type FindByUserIdDto,
+  type FindChannelByUserIdResponse,
+} from '@contracts/channel';
+import { IDENTITY_SERVICE } from '@infra';
+import { lastValueFrom } from 'rxjs';
+
+import { Inject, Injectable } from '@nestjs/common';
+import type { ClientProxy } from '@nestjs/microservices';
+
+import type { ChannelAdapterPort } from '../../../application/ports/channel.adapter.port';
+
+@Injectable()
+export class ChannelRpcAdapter implements ChannelAdapterPort {
+  constructor(@Inject(IDENTITY_SERVICE) private readonly identityClient: ClientProxy) {}
+
+  public findChannelByUserId(userId: string): Promise<FindChannelByUserIdResponse | null> {
+    const payload: FindByUserIdDto = { userId };
+    return lastValueFrom<FindChannelByUserIdResponse | null>(
+      this.identityClient.send(CHANNEL_PATTERNS.FIND_BY_USER_ID, payload),
+    );
+  }
+}
