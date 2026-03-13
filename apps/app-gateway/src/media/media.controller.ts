f1686d20e498ef636eccde4427808e4ff4dcc3ec
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-13 13:52:17 +0300

    feat(media): extract current user and throw into upload media payload

--- apps/app-gateway/src/media/media.controller.ts
+++ apps/app-gateway/src/media/media.controller.ts
@@ -1,6 +1,8 @@
 import { Authorization } from '@app/modules/auth/decorators/authorization.decorator';
+import { CurrentUserDecorator } from '@app/modules/auth/decorators/current-user.decorator';
 import type { UploadCompleteDto } from '@contracts/media/dto/upload-complete.dto';
 import { UploadMediaDto } from '@contracts/media/dto/upload-media.dto';
+import type { UserDto } from '@contracts/user';
 
 import { Body, Controller, Post } from '@nestjs/common';
 
@@ -12,8 +14,8 @@ export class MediaController {
 
   @Authorization()
   @Post('upload')
-  public getUploadUrl(@Body() dto: UploadMediaDto) {
-    return this.mediaService.getUploadUrl(dto);
+  public getUploadUrl(@Body() dto: UploadMediaDto, @CurrentUserDecorator() user: UserDto) {
+    return this.mediaService.getUploadUrl(dto, user.id);
   }
 
   @Authorization()
--- apps/app-gateway/src/media/media.service.ts
+++ apps/app-gateway/src/media/media.service.ts
@@ -1,6 +1,7 @@
 import type { UploadCompleteDto } from '@contracts/media/dto/upload-complete.dto';
 import type { UploadMediaDto } from '@contracts/media/dto/upload-media.dto';
 import { MEDIA_PATTERNS } from '@contracts/media/patterns/media.patterns';
+import type { UploadMediaRpc } from '@contracts/media/rpc/upload-media.rpc';
 import { MEDIA_SERVICE } from 'libs/infra-constants/src';
 
 import { Inject, Injectable } from '@nestjs/common';
@@ -10,8 +11,9 @@ import type { ClientProxy } from '@nestjs/microservices';
 export class MediaService {
   constructor(@Inject(MEDIA_SERVICE) private readonly mediaClient: ClientProxy) {}
 
-  public getUploadUrl(dto: UploadMediaDto) {
-    return this.mediaClient.send(MEDIA_PATTERNS.UPLOAD, dto);
+  public getUploadUrl(dto: UploadMediaDto, userId: string) {
+    const payload: UploadMediaRpc = { ...dto, userId };
+    return this.mediaClient.send(MEDIA_PATTERNS.UPLOAD, payload);
   }
 
   public uploadComplete(dto: UploadCompleteDto) {
