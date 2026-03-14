91165ee01a8ee6d447319063d2c4d0217904fb7c
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-15 00:15:55 +0300

    feat(media): register prisma video repository to providers

--- apps/media/src/media.module.ts
+++ apps/media/src/media.module.ts
@@ -12,10 +12,12 @@ import { handlers } from './application/handlers/handlers';
 import {
   BROKER_EVENT_BUS_TOKEN,
   CHANNEL_ADAPTER_TOKEN,
+  VIDEO_REPOSITORY_READ_TOKEN,
   VIDEO_REPOSITORY_TOKEN,
 } from './application/ports/tokens';
 import { RabbitMQEventBus } from './infrastructure/broker/rabbitmq/rabbitmq-event-bus';
 import { rabbitMQConfig } from './infrastructure/broker/rabbitmq/rabbitmq.config';
+import { PrismaVideoReadRepository } from './infrastructure/persistence/db/prisma-video.read.repository';
 import { PrismaVideoRepository } from './infrastructure/persistence/db/prisma-video.repository';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { ChannelRpcAdapter } from './infrastructure/rpc/adapters/channel.rpc.adapter';
@@ -47,6 +49,10 @@ import { MediaMessagingController } from './presentation/messaging/media.messagi
     {
       provide: VIDEO_REPOSITORY_TOKEN,
       useClass: PrismaVideoRepository,
+    },
+    {
+      provide: VIDEO_REPOSITORY_READ_TOKEN,
+      useClass: PrismaVideoReadRepository,
     },
     {
       provide: BROKER_EVENT_BUS_TOKEN,
