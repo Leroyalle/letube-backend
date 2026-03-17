a3e0be47b9dff909d48550072e8ca8f513d1f469
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-17 09:01:53 +0300

    feat(stream): improve and add find by stream key

--- apps/stream/src/domain/ports/stream-repository.port.ts
+++ apps/stream/src/domain/ports/stream-repository.port.ts
@@ -1,8 +1,9 @@
 import type { Stream } from '../entities/stream.entity';
 
 export interface StreamRepositoryPort {
-  create(data: Stream): Promise<void>;
+  create(data: Stream): Promise<Stream>;
   delete(id: string): Promise<void>;
   update(data: Stream): Promise<void>;
-  findByChannelId(channelId: string): Promise<string | null>;
+  findByChannelId(channelId: string): Promise<Stream | null>;
+  findByStreamKey(streamKey: string): Promise<Stream | null>;
 }
