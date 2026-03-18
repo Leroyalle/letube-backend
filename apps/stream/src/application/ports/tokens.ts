9c1dd441442f2d723033826cd1cb226b6d1cc8b6
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-18 09:08:30 +0300

    feat(stream): add channel rpc adapter and port

---
+++ apps/stream/src/application/ports/tokens.ts
@@ -0,0 +1,3 @@
+export const STREAM_REPOSITORY_TOKEN = Symbol('stream_repository_token');
+export const CACHE_MANAGER_TOKEN = Symbol('cache_manager_token');
+export const CHANNEL_ADAPTER_TOKEN = Symbol('channel_adapter_token');
---
+++ apps/stream/src/infrastructure/rpc/adapters/channel.rpc.adapter.ts
@@ -0,0 +1,23 @@
+import {
+  CHANNEL_PATTERNS,
+  type FindByUserIdDto,
+  type FindChannelByUserIdResponse,
+} from '@contracts/channel';
+import { CHANNEL_SERVICE } from '@infra';
+import type { ChannelAdapterPort } from 'apps/stream/src/application/ports/channel-adapter.port';
+import { lastValueFrom } from 'rxjs';
+
+import { Inject, Injectable } from '@nestjs/common';
+import type { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class ChannelRpcAdapter implements ChannelAdapterPort {
+  constructor(@Inject(CHANNEL_SERVICE) private readonly channelClient: ClientProxy) {}
+
+  public findChannelByUserId(userId: string): Promise<FindChannelByUserIdResponse | null> {
+    const payload: FindByUserIdDto = { userId };
+    return lastValueFrom<FindChannelByUserIdResponse | null>(
+      this.channelClient.send(CHANNEL_PATTERNS.FIND_BY_USER_ID, payload),
+    );
+  }
+}
