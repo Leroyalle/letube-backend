d5415fcd591627f813b83621e37ef54360cd1942
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-18 14:02:17 +0300

    feat(infra-constants): add stream host, port and service name

--- libs/infra-constants/src/transport/hosts.ts
+++ libs/infra-constants/src/transport/hosts.ts
@@ -2,3 +2,4 @@ export const IDENTITY_HOST = 'localhost';
 export const NOTIFICATION_HOST = 'localhost';
 export const CHANNEL_HOST = 'localhost';
 export const MEDIA_HOST = 'localhost';
+export const STREAM_HOST = 'localhost';
