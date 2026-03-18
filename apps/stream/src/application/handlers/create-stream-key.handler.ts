4c0524e0acd94fa5ae4561bdf081c587ce0f2e6c
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-18 09:22:51 +0300

    feat(stream): add create stream handler

---
+++ apps/stream/src/application/handlers/create-stream-key.handler.ts
@@ -0,0 +1,29 @@
+import { randomUUID } from 'crypto';
+
+import { Inject } from '@nestjs/common';
+import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
+
+import { CreateStreamKeyCommand } from '../commands/create-stream-key.command';
+import type { CacheManagerPort } from '../ports/cache-manager.port';
+import type { ChannelAdapterPort } from '../ports/channel-adapter.port';
+import { CACHE_MANAGER_TOKEN, CHANNEL_ADAPTER_TOKEN } from '../ports/tokens';
+
+@CommandHandler(CreateStreamKeyCommand)
+export class CreateStreamKeyHandler implements ICommandHandler<CreateStreamKeyCommand> {
+  constructor(
+    @Inject(CACHE_MANAGER_TOKEN) private readonly cacheManager: CacheManagerPort,
+    @Inject(CHANNEL_ADAPTER_TOKEN) private readonly channelAdapter: ChannelAdapterPort,
+  ) {}
+
+  public async execute(command: CreateStreamKeyCommand) {
+    const channel = await this.channelAdapter.findChannelByUserId(command.userId);
+
+    if (!channel) throw new Error('Channel not found');
+
+    const streamKey = randomUUID();
+
+    await this.cacheManager.add(`streamKey:${streamKey}`, channel.id, 3600);
+
+    return { streamKey };
+  }
+}
