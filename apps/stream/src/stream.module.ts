7aad15482a18ac7b8a0d9a5483c0993a6fa3c75d
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-18 12:23:53 +0300

    feat(stream): register channel adapter, prisma and channel client

--- apps/stream/src/stream.module.ts
+++ apps/stream/src/stream.module.ts
@@ -1,21 +1,41 @@
 import { RedisModule } from '@app/infra-core/redis/redis.module';
+import { CHANNEL_HOST, CHANNEL_PORT, CHANNEL_SERVICE } from '@infra';
 import { join } from 'path';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
-import { CACHE_MANAGER_TOKEN, STREAM_REPOSITORY_TOKEN } from './application/ports/tokens';
+import {
+  CACHE_MANAGER_TOKEN,
+  CHANNEL_ADAPTER_TOKEN,
+  STREAM_REPOSITORY_TOKEN,
+} from './application/ports/tokens';
 import { StreamRepository } from './infrastructure/persistence/db/stream.repository';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { RedisAdapter } from './infrastructure/redis/redis.adapter';
+import { ChannelRpcAdapter } from './infrastructure/rpc/adapters/channel.rpc.adapter';
 import { StreamController } from './presentation/http/stream.controller';
 
 @Module({
   imports: [
+    PrismaModule,
     RedisModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: join(process.cwd(), 'apps', 'stream', '.env'),
     }),
+
+    ClientsModule.register([
+      {
+        name: CHANNEL_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: CHANNEL_HOST,
+          port: CHANNEL_PORT,
+        },
+      },
+    ]),
   ],
   providers: [
     {
@@ -25,6 +45,10 @@ import { StreamController } from './presentation/http/stream.controller';
     {
       provide: CACHE_MANAGER_TOKEN,
       useClass: RedisAdapter,
+    },
+    {
+      provide: CHANNEL_ADAPTER_TOKEN,
+      useClass: ChannelRpcAdapter,
     },
   ],
   controllers: [StreamController],
