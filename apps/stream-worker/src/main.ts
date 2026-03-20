0950730333ddf31ee8cb5ab77f36a546c7776576
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-20 08:41:31 +0300

    feat(stream-worker): connect to rabbitmq

--- apps/stream-worker/src/main.ts
+++ apps/stream-worker/src/main.ts
@@ -1,9 +1,25 @@
+import { STREAM_BROKER_QUEUES } from '@contracts/stream/queues/broker.queues';
+
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 
 import { StreamWorkerModule } from './stream-worker.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(StreamWorkerModule);
-  await app.listen(process.env.port ?? 3000);
+  const config = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [config.getOrThrow<string>('RMQ_URL')],
+      queue: STREAM_BROKER_QUEUES.published,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.init();
 }
+
 void bootstrap();
