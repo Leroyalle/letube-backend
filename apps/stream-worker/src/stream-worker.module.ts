4b8be248295ac36df9a355a9d4e3dd19c87033cd
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-20 09:46:55 +0300

    feat(stream-worker): register DI graph

--- apps/stream-worker/src/stream-worker.module.ts
+++ apps/stream-worker/src/stream-worker.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
+import { S3Module } from '@app/infra-core/s3/s3.module';
+import { join } from 'path';
 
-import { StreamWorkerController } from './stream-worker.controller';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
+
+import { STREAM_PROCESSOR_TOKEN } from './application/ports/tokens';
+import { StreamProcessor } from './infrastructure/stream-processor/stream.processor';
+import { StreamWorkerController } from './presentation/messaging/stream-worker.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    S3Module,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), 'apps', 'stream-worker', '.env'),
+    }),
+    CqrsModule,
+  ],
   controllers: [StreamWorkerController],
+  providers: [
+    {
+      provide: STREAM_PROCESSOR_TOKEN,
+      useClass: StreamProcessor,
+    },
+  ],
 })
 export class StreamWorkerModule {}
