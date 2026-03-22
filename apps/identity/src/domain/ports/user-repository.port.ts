2a2e69b29ddef23c83d95c5006ee620fe658ce91
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-23 00:56:51 +0300

    feat(user): add mapper

--- apps/identity/src/domain/ports/user-repository.port.ts
+++ apps/identity/src/domain/ports/user-repository.port.ts
@@ -1,7 +1,7 @@
 import type { User } from '../entities/user.entity';
 
 export interface UserRepositoryPort {
-  findUserByEmail(email: string): Promise<User>;
+  findUserByEmail(email: string): Promise<User | null>;
   create: (data: User) => Promise<User>;
   findById: (id: string) => Promise<User | null>;
   update: (data: User) => Promise<void>;
