838e50e5cbc79f27d170fa5e751ce667210fb53c
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-23 00:56:18 +0300

    feat(user): update entity, add some fields

--- apps/identity/src/domain/entities/user.entity.ts
+++ apps/identity/src/domain/entities/user.entity.ts
@@ -3,10 +3,25 @@ interface Props {
   name: string;
   email: string;
   password: string;
+  isBanned: boolean;
+  isVerified: boolean;
+  role: 'USER' | 'ADMIN' | 'MODERATOR';
 }
 
 export class User {
-  constructor(private readonly props: Props) {}
+  private constructor(public readonly props: Props) {}
+
+  public static create(props: Props) {
+    return new User({
+      email: props.email,
+      id: props.id,
+      name: props.name,
+      password: props.password,
+      isBanned: props.isBanned,
+      isVerified: props.isVerified,
+      role: props.role,
+    });
+  }
 
   public changePassword(hashedPassword: string) {
     this.props.password = hashedPassword;
