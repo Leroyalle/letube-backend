300b10545e894aec71807160ca28632c44c393de
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-22 14:41:45 +0300

    feat(identity): add refresh token implementation and port

---
+++ apps/identity/src/application/ports/refresh-token-service.port.ts
@@ -0,0 +1,9 @@
+import type { ExpiresData, SuccessLoginDto } from '@contracts/auth';
+
+export interface RefreshTokenServicePort {
+  generate(): SuccessLoginDto['refreshData'];
+  generateAndHash(): Promise<string>;
+  hash(token: string): Promise<string>;
+  verify(token: string, hash: string): Promise<boolean>;
+  getExpires(days: number): ExpiresData;
+}
