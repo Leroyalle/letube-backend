b2c55db14c9484f45893aeb9202dbdf49f89e82e
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-23 15:20:59 +0300

    feat(identity): add find all users query and handler

--- apps/identity/src/application/ports/tokens.ts
+++ apps/identity/src/application/ports/tokens.ts
@@ -2,6 +2,7 @@ export const ACCESS_SERVICE_TOKEN = Symbol('access_service_token');
 export const REFRESH_SERVICE_TOKEN = Symbol('refresh_service_token');
 
 export const USER_REPOSITORY_TOKEN = Symbol('user_repository_token');
+export const USER_READ_REPOSITORY_TOKEN = Symbol('user_read_repository_token');
 export const VERIFICATION_CODE_REPOSITORY_TOKEN = Symbol('verification_code_repository_token');
 export const REFRESH_TOKEN_REPOSITORY_TOKEN = Symbol('refresh_token_repository_token');
 
