1e68743abd0073777c45efc5c722bee8bdce95dc
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-23 16:35:31 +0300

    feat(identity): register all providers, controllers and update imports

--- apps/identity/src/identity.module.ts
+++ apps/identity/src/identity.module.ts
@@ -3,20 +3,98 @@ import { join } from 'path';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
+import { JwtModule } from '@nestjs/jwt';
 
-import { AuthModule } from './auth/auth.module';
+import {
+  ACCESS_SERVICE_TOKEN,
+  BROKER_EVENT_BUS_TOKEN,
+  PASSWORD_HASHER_TOKEN,
+  REFRESH_SERVICE_TOKEN,
+  REFRESH_TOKEN_REPOSITORY_TOKEN,
+  USER_READ_REPOSITORY_TOKEN,
+  USER_REPOSITORY_TOKEN,
+  VERIFICATION_CODE_REPOSITORY_TOKEN,
+} from './application/ports/tokens';
+import type { ClockPort } from './domain/ports/clock.port';
+import type { CodeGeneratorPort } from './domain/ports/code-generator.port';
+import { CLOCK_TOKEN, CODE_GENERATOR_TOKEN } from './domain/ports/tokens';
+import { VerificationCodeService } from './domain/services/verification-code.service';
+import { RabbitMQEventBus } from './infrastructure/broker/rabbitmq/rabbitmq-event-bus';
 import { rabbitMQConfig } from './infrastructure/broker/rabbitmq/rabbitmq.config';
-import { UserModule } from './user/user.module';
+import { RefreshTokenRepository } from './infrastructure/persistence/db/refresh-token/refresh-token.repository';
+import { UserReadRepository } from './infrastructure/persistence/db/user/user.read.repository';
+import { UserRepository } from './infrastructure/persistence/db/user/user.repository';
+import { VerificationCodeRepository } from './infrastructure/persistence/db/verification-code/verification-code.repository';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
+import { ArgonPasswordHasher } from './infrastructure/security/password-hasher/argon-password.hasher';
+import { CodeGenerator } from './infrastructure/system/code.generator';
+import { SystemClockService } from './infrastructure/system/system-clock.service';
+import { AccessTokenService } from './infrastructure/token/access-token.service';
+import { RefreshTokenService } from './infrastructure/token/refresh-token.service';
+import { IdentityCommandsController } from './presentation/commands/identity.commands.controller';
+import { IdentityQueriesController } from './presentation/queries/identity.queries.controller';
 
 @Module({
   imports: [
+    CqrsModule,
+    PrismaModule,
+    JwtModule,
     RabbitMQModule.registerAsync(rabbitMQConfig),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: join(process.cwd(), 'apps', 'identity', '.env'),
     }),
-    AuthModule,
-    UserModule,
+  ],
+  controllers: [IdentityCommandsController, IdentityQueriesController],
+  providers: [
+    {
+      provide: USER_REPOSITORY_TOKEN,
+      useClass: UserRepository,
+    },
+    {
+      provide: USER_READ_REPOSITORY_TOKEN,
+      useClass: UserReadRepository,
+    },
+    {
+      provide: VERIFICATION_CODE_REPOSITORY_TOKEN,
+      useClass: VerificationCodeRepository,
+    },
+    {
+      provide: ACCESS_SERVICE_TOKEN,
+      useClass: AccessTokenService,
+    },
+    {
+      provide: REFRESH_SERVICE_TOKEN,
+      useClass: RefreshTokenService,
+    },
+    {
+      provide: REFRESH_TOKEN_REPOSITORY_TOKEN,
+      useClass: RefreshTokenRepository,
+    },
+    {
+      provide: PASSWORD_HASHER_TOKEN,
+      useClass: ArgonPasswordHasher,
+    },
+    {
+      provide: BROKER_EVENT_BUS_TOKEN,
+      useClass: RabbitMQEventBus,
+    },
+    {
+      provide: CODE_GENERATOR_TOKEN,
+      useClass: CodeGenerator,
+    },
+    {
+      provide: CLOCK_TOKEN,
+      useClass: SystemClockService,
+    },
+    {
+      provide: VerificationCodeService,
+      useFactory: (clockService: ClockPort, codeGenerator: CodeGeneratorPort) => {
+        return new VerificationCodeService(clockService, codeGenerator);
+      },
+      inject: [CLOCK_TOKEN, CODE_GENERATOR_TOKEN],
+    },
   ],
 })
 export class IdentityModule {}
