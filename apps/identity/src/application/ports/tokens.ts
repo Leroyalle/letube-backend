893de486e3306cc9651bc6b79b29a860dcc555d9
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-23 11:40:14 +0300

    feat(identity): add application ports

---
+++ apps/identity/src/application/ports/tokens.ts
@@ -0,0 +1,8 @@
+export const ACCESS_SERVICE_TOKEN = Symbol('access_service_token');
+export const REFRESH_SERVICE_TOKEN = Symbol('refresh_service_token');
+
+export const USER_REPOSITORY_TOKEN = Symbol('user_repository_token');
+export const VERIFICATION_CODE_SERVICE_TOKEN = Symbol('verification_code_service_token');
+export const REFRESH_TOKEN_REPOSITORY_TOKEN = Symbol('refresh_token_repository_token');
+
+export const PASSWORD_HASHER_TOKEN = Symbol('password_hasher_token');
