61fa2a5c92552b9949020738ce449cb7254288c9
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-24 23:56:45 +0300

    feat(chat): add message cache event handler

---
+++ apps/chat/src/application/handlers/message-cache.handler.ts
@@ -0,0 +1,36 @@
+import { CACHE_TOKEN } from '@app/abstractions/cache/cache.token';
+import type { ClockPort } from '@app/abstractions/system/time/clock.port';
+import { CLOCK_TOKEN } from '@app/abstractions/system/time/clock.token';
+
+import { Inject } from '@nestjs/common';
+import { EventsHandler } from '@nestjs/cqrs';
+
+import { MessageCreatedEvent } from '../events/message-created.event';
+import type { CacheManagerPort } from '../ports/cache-manager.port';
+
+@EventsHandler(MessageCreatedEvent)
+export class MessageCacheHandler {
+  constructor(
+    @Inject(CACHE_TOKEN) private readonly cacheManager: CacheManagerPort,
+    @Inject(CLOCK_TOKEN) private readonly clockService: ClockPort,
+  ) {}
+
+  public async handle(event: MessageCreatedEvent) {
+    const key = `${event.props.receiverId}`;
+
+    await this.cacheManager.zadd(
+      key,
+      this.clockService.nowMs(),
+      JSON.stringify({
+        id: event.props.id,
+        content: event.props.content,
+        receiverId: event.props.receiverId,
+        senderId: event.props.senderId,
+      }),
+    );
+
+    await this.cacheManager.zremrangebyrank(key, 0, -101);
+
+    await this.cacheManager.expire(key, 3600);
+  }
+}
--- apps/chat/src/application/handlers/send-message.handler.ts
+++ apps/chat/src/application/handlers/send-message.handler.ts
@@ -1,23 +1,14 @@
-import { CACHE_TOKEN } from '@app/abstractions/cache/cache.token';
 import { Message } from 'apps/chat/src/domain/entities/message.entity';
 import { randomUUID } from 'crypto';
 
-import { Inject } from '@nestjs/common';
 import { CommandHandler, type EventBus, type ICommandHandler } from '@nestjs/cqrs';
 
 import { SendMessageCommand } from '../commands/send-message.command';
 import { MessageCreatedEvent } from '../events/message-created.event';
-import type { BrokerEventBusPort } from '../ports/broker-event-bus.port';
-import type { CacheManagerPort } from '../ports/cache-manager.port';
-import { BROKER_EVENT_BUS_TOKEN } from '../ports/tokens';
 
 @CommandHandler(SendMessageCommand)
 export class SendMessageHandler implements ICommandHandler<SendMessageCommand> {
-  constructor(
-    @Inject(CACHE_TOKEN) private readonly cacheManager: CacheManagerPort,
-    private readonly eventBus: EventBus,
-    @Inject(BROKER_EVENT_BUS_TOKEN) private readonly brokerEventBus: BrokerEventBusPort,
-  ) {}
+  constructor(private readonly eventBus: EventBus) {}
 
   public async execute(command: SendMessageCommand) {
     const message = Message.create({
