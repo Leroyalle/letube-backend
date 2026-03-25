fe4025d03c2a52de55390f323e801180868c9ffe
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-26 00:26:01 +0300

    feat(gateway): add chat gateway and client service

---
+++ apps/app-gateway/src/chat/presentation/ws/chat.gateway.ts
@@ -0,0 +1,37 @@
+import type { MessageDto } from '@contracts/chat/dto/message.dto';
+import type { Server, Socket } from 'socket.io';
+
+import {
+  ConnectedSocket,
+  MessageBody,
+  type OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+
+import { ChatClientService } from '../../infrastructure/transport/chat-client.service';
+import { ChatEmitter } from '../../infrastructure/transport/ws/chat.emitter';
+
+@WebSocketGateway()
+// implements OnGatewayConnection, OnGatewayDisconnect
+export class ChatGateway implements OnGatewayInit {
+  constructor(
+    private readonly chatService: ChatClientService,
+    private readonly chatEmitter: ChatEmitter,
+  ) {}
+
+  public afterInit(server: Server) {
+    return this.chatEmitter.setServer(server);
+  }
+
+  // public handleConnection(client: Socket) {
+  //   return this.chatService.handleConnection(client);
+  // }
+
+  // public handleDisconnect(client: Socket) {}
+
+  @SubscribeMessage('send-message')
+  public handleMessage(@ConnectedSocket() client: Socket, @MessageBody() payload: MessageDto) {
+    return this.chatService.sendMessage(client.data.id as string, payload);
+  }
+}
