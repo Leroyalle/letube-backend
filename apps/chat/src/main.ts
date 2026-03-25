2cf99b3af4d3ad249a83397ee34e07ec9b4c3ad1
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-25 14:32:52 +0300

    feat(chat): connect to RMQ

--- apps/chat/src/main.ts
+++ apps/chat/src/main.ts
@@ -1,9 +1,23 @@
+import { CHAT_QUEUES } from '@contracts/chat/queues/broker.queues';
+
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { type RmqOptions, Transport } from '@nestjs/microservices';
 
 import { ChatModule } from './chat.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ChatModule);
+
+  const config = app.get(ConfigService);
+
+  app.connectMicroservice<RmqOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [config.getOrThrow<string>('RMQ_URL')],
+      queue: CHAT_QUEUES.COMMANDS,
+    },
+  });
   await app.listen(process.env.port ?? 3000);
 }
 void bootstrap();
