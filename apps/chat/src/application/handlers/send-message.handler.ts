6a44a634c17c891a4d1b56e653814041fcecdf3e
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-25 18:11:21 +0300

    feat(chat): improve send message handler, use clock service

--- apps/chat/src/application/handlers/send-message.handler.ts
+++ apps/chat/src/application/handlers/send-message.handler.ts
@@ -1,6 +1,9 @@
+import type { ClockPort } from '@app/abstractions/system/time/clock.port';
+import { CLOCK_TOKEN } from '@app/abstractions/system/time/clock.token';
 import { Message } from 'apps/chat/src/domain/entities/message.entity';
 import { randomUUID } from 'crypto';
 
+import { Inject } from '@nestjs/common';
 import { CommandHandler, type EventBus, type ICommandHandler } from '@nestjs/cqrs';
 
 import { SendMessageCommand } from '../commands/send-message.command';
@@ -8,7 +11,10 @@ import { MessageCreatedEvent } from '../events/message-created.event';
 
 @CommandHandler(SendMessageCommand)
 export class SendMessageHandler implements ICommandHandler<SendMessageCommand> {
-  constructor(private readonly eventBus: EventBus) {}
+  constructor(
+    private readonly eventBus: EventBus,
+    @Inject(CLOCK_TOKEN) private readonly clockService: ClockPort,
+  ) {}
 
   public async execute(command: SendMessageCommand) {
     const message = Message.create({
@@ -16,14 +22,18 @@ export class SendMessageHandler implements ICommandHandler<SendMessageCommand> {
       content: command.props.content,
       receiverId: command.props.receiverId,
       senderId: command.props.senderId,
+      createdAt: this.clockService.now(),
     });
+
+    const snapshot = message.snapshot();
 
     this.eventBus.publish(
       new MessageCreatedEvent({
-        id: message.props.id,
-        content: message.props.content,
-        receiverId: message.props.receiverId,
-        senderId: message.props.senderId,
+        id: snapshot.id,
+        content: snapshot.content,
+        receiverId: snapshot.receiverId,
+        senderId: snapshot.senderId,
+        createdAt: snapshot.createdAt,
       }),
     );
     return Promise.resolve();
