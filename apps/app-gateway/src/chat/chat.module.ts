e9d733bafb3c49a4e7f363b34b7af0b78772147f
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-27 14:02:43 +0300

    feat(chat): register cqrs module. events controller and handler to gateway module

--- apps/app-gateway/src/chat/chat.module.ts
+++ apps/app-gateway/src/chat/chat.module.ts
@@ -3,7 +3,9 @@ import { join } from 'path';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
 
+import { handlers } from './application/commands/handlers';
 import { BROKER_EVENT_BUS_TOKEN } from './application/ports/token';
 import { ChatClientService } from './application/use-cases/chat-client.service';
 import { RabbitMQEventBus } from './infrastructure/broker/rabbitmq/rabbitmq-event-bus';
@@ -14,17 +16,19 @@ import { ChatGateway } from './presentation/ws/chat.gateway';
 
 @Module({
   imports: [
+    CqrsModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: join(process.cwd(), 'apps', 'app-gateway', '.env'),
     }),
     RabbitMQModule.registerAsync(rabbitMQConfig),
   ],
+  controllers: [ChatEventsController],
   providers: [
     ChatGateway,
     ChatClientService,
     ChatEmitter,
-    ChatEventsController,
+    ...handlers,
     {
       provide: BROKER_EVENT_BUS_TOKEN,
       useClass: RabbitMQEventBus,
