19760f8a34e57bea425b7930a690d1cd765c2ba1
Leroyalle <nikolajmelonov@yandex.ru>
2026-03-27 13:35:12 +0300

    fix(chat): replace event bus to command

--- apps/chat/src/presentation/events/chat.events.controller.ts
+++ apps/chat/src/presentation/events/chat.events.controller.ts
@@ -2,18 +2,18 @@ import { CHAT_PATTERNS } from '@contracts/chat/patterns/patterns';
 import type { SendMessageRpcContract } from '@contracts/chat/rpc/send-message.rpc';
 
 import { Controller } from '@nestjs/common';
-import { EventBus } from '@nestjs/cqrs';
+import { CommandBus } from '@nestjs/cqrs';
 import { EventPattern } from '@nestjs/microservices';
 
 import { SendMessageCommand } from '../../application/commands/send-message.command';
 
 @Controller()
 export class ChatEventsController {
-  constructor(private readonly eventBus: EventBus) {}
+  constructor(private readonly commandBus: CommandBus) {}
 
-  @EventPattern(CHAT_PATTERNS.MESSAGE_CREATED)
+  @EventPattern(CHAT_PATTERNS.MESSAGE_CREATE)
   public messageCreated(data: SendMessageRpcContract) {
-    return this.eventBus.publish(
+    return this.commandBus.execute(
       new SendMessageCommand({
         content: data.content,
         senderId: data.senderId,
